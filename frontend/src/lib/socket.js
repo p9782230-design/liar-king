4a08b3c4a750a948d5569d5741da068cef9792d0
HC0912 <p9782230@gmail.com>
2025-12-29 07:33:03 +0800

    fix socket url for render

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  import.meta.env.DEV
-    ? `http://${window.location.hostname}:3001`
-    : window.location.origin;
+  import.meta.env.VITE_SERVER_URL || "https://liar-king-server.onrender.com";
+
+console.log("SOCKET_URL =", SOCKET_URL);
 
 export const socket = io(SOCKET_URL, {
   transports: ["websocket"],
